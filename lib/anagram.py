1e41bce733384c69ea4f3a68f16e653f04912b23
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2024-09-30 12:40:56 -0400

    initialized and added match method to anagram class

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+
+
+    def match(self,text_list):
+        match_list = [text for text in text_list if sorted(text) == sorted(self.word) ]
+        return match_list 
+        
+        
+                
